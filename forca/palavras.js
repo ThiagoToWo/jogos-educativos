b5177e37d01dbb07787ec113683f30ece56320f9
ThiagoToWo <towo497@gmail.com>
2021-09-03 01:13:58 -0300

    100 palavras

--- forca/palavras.js
+++ forca/palavras.js
@@ -1,1 +1,17 @@
-var palavras = ['pau', 'cu', 'boceta'];
+var palavras = ['Amarelo','Amiga','Amor','Ave','Avião','Avó','Balão','Bebê','Bolo',
+				'Branco','Cama','Caneca','Celular','Clube','Copo','Doce','Elefante',
+				'Escola','Estojo','Faca','Foto','Garfo','Geleia','Girafa','Janela',
+				'Limonada','Mãe','Meia','Noite','Óculos','ônibus','Ovo','Pai','Pão',
+				'Parque','Passarinho','Peixe','Pijama','Rato','Umbigo','Acender',
+				'Afilhado','Ardiloso','Áspero','Assombração','Asterisco','Basquete',
+				'Caminho','Champanhe','Chiclete','Chuveiro','Coelho','Contexto',
+				'Convivência','Coração','Desalmado','Eloquente','Esfirra','Esquerdo',
+				'Exceção','Fugaz','Gororoba','Heterossexual','Horrorizado','Impacto',
+				'Independência','Modernidade','Oftalmologista','Otorrinolaringologista',
+				'Paralelepípedo','Pororoca','Prognósticio','Quarentena','Quimera',
+				'Refeição','Reportagem','Sino','Taciturno','Tênue','Visceral','Afobado',
+				'Amendoim','Banheiro','Caatinga','Cachorro','Campeonato','Capricórnio',
+				'Catapora','Corrupção','Crepúsculo','Empenhado','Esparadrapo','Forca',
+				'Galáxia','História','Magenta','Manjericão','Menta','Moeda','Oração',
+				'Paçoca','Palavra','Pedreiro','Pneumonia','Pulmão','Rotatória','Serenata',
+				'Transeunte','Trilogia','Xícara'];
